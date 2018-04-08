d2c4a9217cec57881519517186a0b72189800457
Nicolas Surleraux <nsurleraux@gmail.com>
2018-04-08 12:32:35 +0200

    Add possibility to set consul address at runtime

--- etc/cont-init.d/00-init-consul-template.sh
+++ etc/cont-init.d/00-init-consul-template.sh
@@ -2,4 +2,8 @@
 
 ln -s /var/run/s6/services/haproxy1 /haproxy-current
 ln -s /var/run/s6/services/haproxy2 /haproxy-alt
-consul-template -config /consul-template/config.d -once
+if [ -z $CONSUL_ADDR ]; then
+  consul-template -config /consul-template/config.d -consul-addr "$CONSUL_ADDR" -once
+else
+  consul-template -config /consul-template/config.d -once
+fi
